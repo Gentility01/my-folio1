34d1d514b01418b3afd7ef66cf5afea5788c7e83
Gentility01 <mastergentility5@gmail.com>
2023-01-06 07:58:32 +0100

    updated

--- folio/settings/prod.py
+++ folio/settings/prod.py
@@ -4,7 +4,7 @@ import os
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
