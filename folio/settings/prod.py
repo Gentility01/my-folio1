219c333eb55231fa963039d5da23dc20055206c0
Gentility01 <mastergentility5@gmail.com>
2023-01-06 06:47:18 +0100

    updated

--- folio/settings/prod.py
+++ folio/settings/prod.py
@@ -4,9 +4,9 @@ import os
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'myfolio.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
