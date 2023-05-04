2044333edb4de90b45b6c4fff19de02df207d07e
Gentility01 <mastergentility5@gmail.com>
2023-05-04 20:45:54 +0100

    more email updates

--- folio/settings/prod.py
+++ folio/settings/prod.py
@@ -27,7 +27,7 @@ DATABASES = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
